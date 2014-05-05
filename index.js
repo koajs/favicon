0a02f5f18f94bdc217d32df7e930f0c9148cfe8b
TJ Holowaychuk <tj@vision-media.ca>
2014-05-05 16:47:03 -0700

    ocd

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+/**
+ * Module dependencies.
+ */
+
 var resolve = require('path').resolve;
 var fs = require('co-fs');
 
@@ -12,28 +17,29 @@ var fs = require('co-fs');
 
 module.exports = function (path, options){
   var icon;
-  
+
   if (path) path = resolve(path);
   options = options || {};
-  
+
   var maxAge = options.maxAge == null
     ? 86400000
     : Math.min(Math.max(0, options.maxAge), 31556926000);
-  
+
   return function *favicon(next){
     if ('/favicon.ico' != this.path) return yield next;
+
     if (!path) return;
-    
+
     if ('GET' !== this.method && 'HEAD' !== this.method) {
       this.status = 'OPTIONS' == this.method ? 200 : 405;
       this.set('Allow', 'GET, HEAD, OPTIONS');
       return;
     }
-    
+
     if (!icon) icon = yield fs.readFile(path);
-    
+
+    this.set('Cache-Control', 'public, max-age=' + (maxAge / 1000 | 0));
     this.type = 'image/x-icon';
-    this.set('Cache-Control', 'public, max-age=' + ~~(maxAge / 1000));
     this.body = icon;
   };
 };
