bf8740615473fd70fbb9f6862ad4754112ecef19
gyson <eilian.yunsong@gmail.com>
2015-11-02 21:09:36 -0500

    support koa 2.x

--- test/index.js
+++ test/index.js
@@ -1,80 +1,81 @@
-var request = require('supertest');
-var join = require('path').join;
-var favicon = require('..');
-var koa = require('koa');
-var fs = require('fs');
+'use strict';
+
+const request = require('supertest');
+const join = require('path').join;
+const favicon = require('..');
+const Koa = require('koa');
+const fs = require('fs');
 
 describe('favicon()', function(){
-  var path = join(__dirname, 'fixtures', 'favicon.ico');
-  
+  const path = join(__dirname, 'fixtures', 'favicon.ico');
+
   it('should only respond on /favicon.ico', function(done){
-    var app = koa();
-    
+    const app = new Koa();
+
     app.use(favicon(path));
-    
-    app.use(function *(next){
-      (this.body == null).should.be.true;
-      (this.get('Content-Type') == null).should.be.true;
-      this.body = 'hello';
+
+    app.use((ctx, next) => {
+      (ctx.body == null).should.be.true;
+      (ctx.get('Content-Type') == '').should.be.true;
+      ctx.body = 'hello';
     });
-    
+
     request(app.listen())
     .get('/')
     .expect('hello', done);
   });
-  
+
   it('should 404 if `path` is missing', function(done){
-    var app = koa();
+    const app = new Koa();
     app.use(favicon());
     request(app.listen())
     .post('/favicon.ico')
     .expect(404, done);
   });
-  
+
   it('should not accept POST requests', function(done){
-    var app = koa();
+    const app = new Koa();
     app.use(favicon(path));
-    
+
     request(app.listen())
     .post('/favicon.ico')
     .expect('Allow', 'GET, HEAD, OPTIONS')
     .expect(405, done);
   });
-  
+
   it('should send the favicon', function(done){
-    var body = fs.readFileSync(path);
-    
-    var app = koa();
+    const body = fs.readFileSync(path);
+
+    const app = new Koa();
     app.use(favicon(path));
-    
+
     request(app.listen())
     .get('/favicon.ico')
-    .expect(200)
     .expect('Content-Type', 'image/x-icon')
-    .expect(body.toString(), done);
+    .expect(200, body, done);
   });
-  
+
   it('should set cache-control headers', function(done){
-    var app = koa();
+    const app = new Koa();
     app.use(favicon(path));
     request(app.listen())
     .get('/favicon.ico')
     .expect('Cache-Control', 'public, max-age=86400')
     .expect(200, done);
   });
-  
+
   describe('options.maxAge', function(){
     it('should set max-age', function(done){
-      var app = koa();
+      const app = new Koa();
       app.use(favicon(path, { maxAge: 5000 }));
       request(app.listen())
       .get('/favicon.ico')
       .expect('Cache-Control', 'public, max-age=5')
       .expect(200, done);
     });
-    
+
     it('should accept 0', function(done){
-      var app = koa();
+      const app = new Koa();
       app.use(favicon(path, { maxAge: 0 }));
       request(app.listen())
       .get('/favicon.ico')
@@ -83,7 +84,7 @@ describe('favicon()', function(){
     });
 
     it('should be valid delta-seconds', function(done){
-      var app = koa();
+      const app = new Koa();
       app.use(favicon(path, { maxAge: 1234 }));
       request(app.listen())
       .get('/favicon.ico')
@@ -92,7 +93,7 @@ describe('favicon()', function(){
     });
 
     it('should floor at 0', function(done){
-      var app = koa();
+      const app = new Koa();
       app.use(favicon(path, { maxAge: -4000 }));
       request(app.listen())
       .get('/favicon.ico')
@@ -101,7 +102,7 @@ describe('favicon()', function(){
     });
 
     it('should ceil at 31556926', function(done){
-      var app = koa();
+      const app = new Koa();
       app.use(favicon(path, { maxAge: 900000000000 }));
       request(app.listen())
       .get('/favicon.ico')
@@ -110,7 +111,7 @@ describe('favicon()', function(){
     });
 
     it('should accept Infinity', function(done){
-      var app = koa();
+      const app = new Koa();
       app.use(favicon(path, { maxAge: Infinity }));
       request(app.listen())
       .get('/favicon.ico')
