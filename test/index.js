201c130cc2bde24237fdce3e71d4baec6c75097a
Jonathan Ong <jonathanrichardong@gmail.com>
2016-02-27 13:08:15 -0800

    remove

--- test/index.js
+++ test/index.js
@@ -1,28 +1,29 @@
 var request = require('supertest');
 var join = require('path').join;
+var assert = require('assert');
 var favicon = require('..');
 var koa = require('koa');
 var fs = require('fs');
 
 describe('favicon()', function(){
   var path = join(__dirname, 'fixtures', 'favicon.ico');
-  
+
   it('should only respond on /favicon.ico', function(done){
     var app = koa();
-    
+
     app.use(favicon(path));
-    
+
     app.use(function *(next){
-      (this.body == null).should.be.true;
-      (this.get('Content-Type') == null).should.be.true;
+      assert(this.body == null);
+      assert(this.get('Content-Type') == null);
       this.body = 'hello';
     });
-    
+
     request(app.listen())
     .get('/')
     .expect('hello', done);
   });
-  
+
   it('should 404 if `path` is missing', function(done){
     var app = koa();
     app.use(favicon());
@@ -30,30 +31,30 @@ describe('favicon()', function(){
     .post('/favicon.ico')
     .expect(404, done);
   });
-  
+
   it('should not accept POST requests', function(done){
     var app = koa();
     app.use(favicon(path));
-    
+
     request(app.listen())
     .post('/favicon.ico')
     .expect('Allow', 'GET, HEAD, OPTIONS')
     .expect(405, done);
   });
-  
+
   it('should send the favicon', function(done){
     var body = fs.readFileSync(path);
-    
+
     var app = koa();
     app.use(favicon(path));
-    
+
     request(app.listen())
     .get('/favicon.ico')
     .expect(200)
     .expect('Content-Type', 'image/x-icon')
     .expect(body.toString(), done);
   });
-  
+
   it('should set cache-control headers', function(done){
     var app = koa();
     app.use(favicon(path));
@@ -62,7 +63,7 @@ describe('favicon()', function(){
     .expect('Cache-Control', 'public, max-age=86400')
     .expect(200, done);
   });
-  
+
   describe('options.maxAge', function(){
     it('should set max-age', function(done){
       var app = koa();
@@ -72,7 +73,7 @@ describe('favicon()', function(){
       .expect('Cache-Control', 'public, max-age=5')
       .expect(200, done);
     });
-    
+
     it('should accept 0', function(done){
       var app = koa();
       app.use(favicon(path, { maxAge: 0 }));
