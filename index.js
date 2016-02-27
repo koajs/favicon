ad7b085e1a8838dda32c910e030b99b7cb929b5e
Jonathan Ong <jonathanrichardong@gmail.com>
2016-02-27 13:26:55 -0800

    lazily read the icon

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ module.exports = function (path, options){
   path = resolve(path);
   options = options || {};
 
-  const icon = fs.readFileSync(path);
+  let icon;
   const maxAge = options.maxAge == null
     ? 86400000
     : Math.min(Math.max(0, options.maxAge), 31556926000);
@@ -38,10 +38,13 @@ module.exports = function (path, options){
     if ('/favicon.ico' != ctx.path) {
       return next();
     }
+
     if ('GET' !== ctx.method && 'HEAD' !== ctx.method) {
       ctx.status = 'OPTIONS' == ctx.method ? 200 : 405;
       ctx.set('Allow', 'GET, HEAD, OPTIONS');
     } else {
+      // lazily read the icon
+      if (!icon) icon = fs.readFileSync(path);
       ctx.set('Cache-Control', cacheControl);
       ctx.type = 'image/x-icon';
       ctx.body = icon;
